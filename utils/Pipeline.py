4a8306e3c33b03395b8ed817d779055320db6c1a
liangjiaxi <11849322@mail.sustech.edu.cn>
2019-04-09 21:36:22 +0800

    修改.gitignore使之忽略original_data文件夹下的东西，不会提交之. 添加了一个pipeline的东西

---
+++ utils/Pipeline.py
@@ -0,0 +1,36 @@
+"""
+梁家熙
+想定义一个类似管道的，
+数据清理的流程
+"""
+from typing import Callable, Dict, List
+
+
+class Pipeline(object):
+    """
+    定义提取的函数。
+    """
+    def __init__(self, function_list: List = None):
+        if function_list:
+            assert isinstance(function_list, list)
+            self.function_lst = function_list
+        else:
+            self.function_lst = []
+            
+    def append_func(self, func: Callable) -> None:
+        if not callable(func):
+            raise TypeError
+        self.function_lst.append(func)
+    
+    def run(self, data: Dict) -> Dict:
+        #我们约定数据能够输入输出是一个格式的，这样才能不断调用函数。
+        for func in self.function_lst:
+            data = func(data)
+        return data
+
+
+if __name__ == '__main__':
+    a=lambda x: x[0]
+    p = Pipeline()
+    p.append_func(a)
+    
